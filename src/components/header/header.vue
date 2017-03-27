c19fd3fcb6d6d83fd59c71c9b6cdf7a04ef95090
unknown <>
2017-03-27 23:58:20 +0800

    头部弹层指向箭头

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,6 +17,14 @@
              <span class="text">{{seller.supports[0].description}}</span>
            </div>
        </div>
+       <div v-if="seller.supports" class="support-count">
+         <span class="count">{{seller.supports.length}}个</span>
+         <i class="icon-keyboard_arrow_right"></i>
+       </div>
+     </div>
+     <div class="bulletin-wrapper">
+       <psan class="bulletin-title"></psan>
+       <span class="bulletin-text">{{seller.bulletin}}</span>
      </div>
   </div>
 </template>
@@ -39,8 +47,9 @@
 
   .header
     color: #fff
-    background: #000
+    background: #999
     .content-wrapper
+      position: relative
       padding: 24px 12px 18px 24px
       font-size: 0
       .avatar
@@ -94,4 +103,21 @@
             line-height: 12px
             font-size: 12px
             vertical-align: top
+      .support-count
+        position: absolute
+        right: 12px
+        bottom: 18px
+        padding: 0 8px
+        height: 24px
+        line-height: 24px
+        border-radius: 14px
+        background: rgba(0,0,0,0.2)
+        text-align: center
+        .count
+          vertical-align: top
+          font-size: 10px
+        .icon-keyboard_arrow_right
+          font-size: 10px
+          line-height: 24px
+          margin-left: 2px
 </style>
